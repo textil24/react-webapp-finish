5141af42877b60fe18e791f1d1f7205cc31b7ed6
textil24 <textilofficial@mail.ru>
2023-02-26 18:07:24 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import {useEffect} from "react";
+
+const tg = window.Telegram.WebApp
+
+function App() {
+
+    useEffect(() => {
+        tg.ready()
+    }, [])
+
+    const onClose = () => {
+        tg.close()
+    }
+
+    return (
+        <div className="App">
+            Это приложение!
+            <button onClick={onClose}>
+                Закрыть
+            </button>
+        </div>
+    );
+}
+
+export default App;
