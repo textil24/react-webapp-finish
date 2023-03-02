56141c4e83a6f23778af2f73f519fa63f7065365
textil24 <textilofficial@mail.ru>
2023-03-02 15:16:16 +0300

    added change answer list choose answer ( FIX)

--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -51,7 +51,7 @@ const Quiz = () => {
     const [clickAnswerList, setClickAnswerList] = useState([])
 
     // Конец таймера
-    const [isEndTimer, setIsEndTimer] = useState(false)
+    const [isEndTimer, setIsEndTimer] = useState(true)
 
     const quizLength = card.quiz.length
     const percentage = step / quizLength * 100
@@ -149,7 +149,7 @@ const Quiz = () => {
 
     const IconAnswerList = ({ index, correctAnswer }) => {
         if (isEndTimer) {
-            if (clickAnswerList[index] === index && clickAnswerItem[0] === correctAnswer) {
+            if (clickAnswerList[index] === index && clickAnswerList[index] === correctAnswer) {
                 return (
                     <svg xmlns="http://www.w3.org/2000/svg" width="19" height="14" viewBox="0 0 19 14" fill="none">
                         <line x1="7.81206" y1="11.2929" x2="17.8121" y2="1.29289" stroke="white" strokeWidth="2"/>
@@ -158,7 +158,7 @@ const Quiz = () => {
                         <line x1="7.81206" y1="12.7071" x2="1.29286" y2="6.1879" stroke="white" strokeWidth="2"/>
                     </svg>
                 )
-            } else if (clickAnswerList[index] === index && clickAnswerItem[0] !== correctAnswer) {
+            } else if (clickAnswerList[index] === index && clickAnswerList[index] !== correctAnswer) {
                 return (
                     <svg xmlns="http://www.w3.org/2000/svg" width="27" height="27" viewBox="0 0 27 27" fill="none">
                         <line x1="6.50519" y1="6.50537" x2="19.5156" y2="19.5158" stroke="white" strokeWidth="2.16465"/>
